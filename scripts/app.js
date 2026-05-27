cad429bbc1be92e92ec13c679f9b1b1aea1bb599
johnreeeyy <johnfrancisco1801@gmail.com>
2026-05-27 14:12:55 +0800

    add a favorite-feature inside a local storage

--- scripts/app.js
+++ scripts/app.js
@@ -2,50 +2,101 @@
 
 let searchValue = "";
 let mythologyFilter = "all";
+let favoritesOnly = false;
+
+let favoriteGods = new Set(JSON.parse(localStorage.getItem("favorites")) || []);
 
 // extract necessary element data from html
 
+// header
 const searchBar = document.querySelector(".searchbar");
 const filterButtons = document.querySelectorAll(".filter-buttons button");
+const favoritesToggle = document.querySelector(".favorites-toggle");
+
+// card container
 const godsContainer = document.querySelector(".gods-container");
+
+// modal
 const modalOverlay = document.querySelector(".modal-overlay");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close-button");
+const modalTitle = modal.querySelector("h1");
+const triviaText = modal.querySelector(".god-trivia p");
+const loreText = modal.querySelector(".god-lore p");
 const domainList = modal.querySelector(".god-domain ul");
 const symbolList = modal.querySelector(".god-symbol ul");
 const modalImg = document.querySelector(".modal .left-content img");
 
+// toggle favorites
+function toggleFavorite(god, button) {
+  const godName = god.name.toLowerCase();
+
+  if (favoriteGods.has(godName)) {
+    favoriteGods.delete(godName);
+    button.textContent = "☆";
+  } else {
+    favoriteGods.add(godName);
+    button.textContent = "★";
+  }
+
+  saveFavorites();
+  applyFilters();
+}
+
+function saveFavorites() {
+  localStorage.setItem("favorites", JSON.stringify([...favoriteGods]));
+}
+
 // render cards dynamically
 
-function renderGods() {
-  godsContainer.innerHTML = "";
+// create card
+function createGodCard(god) {
+  const card = document.createElement("div");
+  const mythologyClass = god.mythology.toLowerCase();
+  card.classList.add("gods-card");
 
-  gods.forEach((god) => {
-    const mythologyClass = god.mythology.toLowerCase();
+  card.dataset.mythology = mythologyClass;
+  card.dataset.name = god.name.toLowerCase();
 
-    const card = document.createElement("div");
-    card.classList.add("gods-card");
-
-    card.dataset.mythology = mythologyClass;
-
-    card.innerHTML = `
+  card.innerHTML = `
       <img src="${god.image}" alt="${god.name}">
 
       <div class="gods-identity">
         <h2 class="gods-name">${god.name}</h2>
+
         <span class="mythology-tag ${mythologyClass}">
           ${god.mythology} Mythology
         </span>
       </div>
 
-      <p>${god.description}</p>
+      <div class="card-footer">
+        <p>${god.description}</p>
+
+        <button class="favorite-button">
+          ${favoriteGods.has(god.name.toLowerCase()) ? "★" : "☆"}
+        </button>
+      </div>
     `;
 
-    godsContainer.appendChild(card);
+  // select button from current card
+  const favoriteBtn = card.querySelector(".favorite-button");
+  favoriteBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    toggleFavorite(god, favoriteBtn);
+  });
 
-    card.addEventListener("click", () => {
-      openModal(god);
-    });
+  // OPEN MODAL
+  card.addEventListener("click", () => {
+    openModal(god);
+  });
+  return card;
+}
+
+function renderGods() {
+  godsContainer.innerHTML = "";
+
+  gods.forEach((god) => {
+    godsContainer.appendChild(createGodCard(god));
   });
 }
 
@@ -57,9 +108,7 @@ modal.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  renderGods();
-});
+renderGods();
 
 // filter using search
 
@@ -89,25 +138,25 @@ function applyFilters() {
   const cards = document.querySelectorAll(".gods-card");
 
   cards.forEach((card) => {
-    const name = card.querySelector("h2").textContent.toLowerCase();
+    const name = card.dataset.name;
     const mythology = card.dataset.mythology;
 
     const matchesSearch = name.includes(searchValue);
+
     const matchesMythology =
       mythologyFilter === "all" || mythology === mythologyFilter;
 
-    if (matchesSearch && matchesMythology) {
-      card.style.display = "";
-    } else {
-      card.style.display = "none";
-    }
+    const matchesFavorites = !favoritesOnly || favoriteGods.has(name);
+
+    card.style.display =
+      matchesSearch && matchesMythology && matchesFavorites ? "" : "none";
   });
 }
 
 // open modal
 
 function openModal(god) {
-  modal.querySelector("h1").textContent = god.name;
+  modalTitle.textContent = god.name;
 
   modalImg.src = god.image;
 
@@ -115,8 +164,9 @@ function openModal(god) {
 
   symbolList.innerHTML = god.symbols.map((s) => `<li>${s}</li>`).join("");
 
-  modal.querySelector(".god-trivia p").textContent = god.trivia;
-  modal.querySelector(".god-lore p").textContent = god.lore;
+  triviaText.textContent = god.trivia;
+
+  loreText.textContent = god.lore;
 
   modalOverlay.classList.add("active");
 
@@ -131,3 +181,12 @@ function closeModal() {
   modalOverlay.classList.remove("active");
   document.body.style.overflow = "auto";
 }
+
+// filter by favorite
+favoritesToggle.addEventListener("click", () => {
+  favoritesOnly = !favoritesOnly;
+
+  favoritesToggle.classList.toggle("active");
+
+  applyFilters();
+});
